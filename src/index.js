2d1ba2b6bcd795c28ee7489fcce5c0548ad3d384
Collins <collinskiarie13@outlook.com>
2023-06-23 14:30:19 +0300

    Completed Task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let taskForm = document.getElementById('create-task-form')
+  let ul=document.getElementById('tasks')
+  taskForm.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    const taskInput=document.querySelector("#new-task-description");
+    const toDo=taskInput.value
+    if(toDo){
+      const newList=document.createElement('li');
+    const btn=document.createElement('button')
+    btn.textContent=  'x'
+    btn.addEventListener('click',deleteButton)
+      newList.innerText=`${toDo} `;
+      newList.append(btn);
+      
+      ul.appendChild(newList)
+      input.value='';
+    }
+
+
+   
+})
+})
+function deleteButton(e){
+  e.target.parentNode.remove()
+}
